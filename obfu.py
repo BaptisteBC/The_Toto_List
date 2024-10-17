5b0b3522443dad76c080e559e0fb203a824e2b1f
macset <73999852+macset@users.noreply.github.com>
2024-10-17 15:36:37 +0200

    Add files via upload
    obfuscation des table sql (pas fini besoin de la bdd finis)
    
    Signed-off-by: macset <73999852+macset@users.noreply.github.com>

---
+++ obfu.py
@@ -0,0 +1,37 @@
+import pymysql
+
+#from testobfu import requetes
+
+# Dictionnaire de mappage des tables
+mtables = {
+    "user": "xner1etn",
+    "password":"frfrce",
+}
+
+
+def obfuscation(requete):
+    # Connexion à la base de données MariaDB
+    conn = pymysql.connect(
+        host='127.0.0.1',
+        user='root',
+        password='root',
+        db='test'
+    )
+
+    cursor = conn.cursor()
+
+    for table_orig in mtables.keys():
+        if table_orig in requete:
+            # Obtenir le nom de la table obfusquée
+            table_obfusquee = mtables[table_orig]
+            # Construire la nouvelle requête
+            requete = requete.replace(table_orig, table_obfusquee)  # Ici, on remplace seulement la table
+            break  # Sortir de la boucle après le premier remplacement
+    # Exécution de la requête obfusquée sur la base de données
+    cursor.execute(requete)
+    resultats = cursor.fetchall()
+
+    # Fermer la connexion
+    conn.close()
+
+    return resultats  # Retourner les résultats
