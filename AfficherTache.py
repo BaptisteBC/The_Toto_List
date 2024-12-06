95bf47a0e3d5bf574d4c6d03b017f14576743497
BaptisteBC <@>
2024-12-06 22:14:54 +0100

    Affichage des taches et sous-taches en fonction de leur ID

--- AfficherTache.py
+++ AfficherTache.py
@@ -1,21 +1,22 @@
 import sys
-import threading
 
 import pymysql
 from PyQt5.QtCore import QCoreApplication
-from PyQt5.QtWidgets import QWidget, QGridLayout, QTextEdit, QApplication, QLabel, QPushButton, QTextBrowser, \
+from PyQt5.QtWidgets import QWidget, QGridLayout, QApplication, QLabel, QPushButton, \
     QListWidget
 
 
-class Principal(QWidget):
-    def __init__(self, host:str='127.0.0.1', user:str='root', password='toto', database:str='thetotodb'):
-        """
+def tache_cliquee():
+    print("ça fonctionne !")
 
-        :type user: str
-        :type host: str
-        :type password: str
-        :type database: str
-        """
+def quitter():
+    QCoreApplication.exit(0)
+
+class Principal(QWidget):
+    """Fenêtre principale d'affichage des tâches.
+    Prends en argument les infos de connexion à la BDD"""
+
+    def __init__(self, host:str='127.0.0.1', user:str='root', password='toto', database:str='thetotodb'):
         super().__init__()
 
         self.setWindowTitle("Page Prinicpale")
@@ -38,37 +39,50 @@ class Principal(QWidget):
         grid.addWidget(self.bouton_quitter)
 
 
-        self.bouton_quitter.clicked.connect(self.quitter)
+        self.bouton_quitter.clicked.connect(quitter)
         self.bouton_actualiser.clicked.connect(self.actualiser)
-        self.taches.itemClicked.connect(self.tache_cliquee)
+        self.taches.itemClicked.connect(tache_cliquee)
 
         self.host = host
         self.user = user
         self.password = password
         self.database = database
 
-        self.cnx = pymysql.connect(host=host, user=user, password=password, database=database)
 
-    def quitter(self):
-        QCoreApplication.exit(0)
+
+
 
     def actualiser(self):
+        """Fonction d'actualisation des tâches.
+        Lorsue le bouton est cliqué, effectue une requête dans la table tâche et récupère le champ 'titre_tache' pour \
+        l'afficher dans la fenêtre principale."""
+
+        self.cnx = pymysql.connect(host=self.host, user=self.user, password=self.password, database=self.database)
         curseur = self.cnx.cursor()
-        curseur.execute("SELECT titre FROM taches;")
-        resultat = curseur.fetchall()
+
+        # Extraction de tous les titres des taches principales
+        curseur.execute("SELECT id_tache,titre_tache FROM taches;")
+        resultache = curseur.fetchall()
+        print(resultache)
+
+        # Extraction de tous les titres des sous taches
+        curseur.execute("SELECT soustache_id_tache,titre_soustache FROM soustaches;")
+        sousresultache = curseur.fetchall()
+        print(sousresultache)
+
         self.taches.clear()
-        for i in range(0, len(resultat)):
-            tache = resultat[i][0]
-        #    self.taches.append(tache)
+
+        for i in range(0, len(resultache)):
+            tache = resultache[i][1]
+
+            #self.taches.append(tache)
             self.taches.addItem(tache)
 
-    def tache_cliquee(self):
-        print("ça fonctionne !")
-
-
-
-
-
+            for j in range(0, len(sousresultache)):
+                if sousresultache[j][0] == resultache[i][0] :
+                    self.taches.addItem(f'|=> {sousresultache[j][1]}')
+        curseur.close()
+        self.cnx.close()
 
 
 if __name__ == "__main__":
