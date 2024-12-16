7e166a007cce1a8990e8705236126736e8fd8aa8
BaptisteBC <@>
2024-12-16 21:58:05 +0100

    Suppression des tâches et sous-taches. Si les taches sont supprimées, les sous-taches sont également modifiées.

--- AfficherTache.py
+++ AfficherTache.py
@@ -55,7 +55,8 @@ class Principal(QWidget):
         resultache = curseur.fetchall()
 
         # Extraction de tous les titres des sous taches
-        curseur.execute("SELECT id_soustache, titre_soustache, soustache_id_tache FROM soustaches;")
+        curseur.execute("SELECT id_soustache, titre_soustache, soustache_id_tache, datesuppression_soustache FROM "
+                        "soustaches;")
         sousresultache = curseur.fetchall()
 
         self.taches.clear()
@@ -66,8 +67,10 @@ class Principal(QWidget):
             else:
                 self.afficherTache(id_tache, titre_tache)
 
-                for id_soustache, titre_soustache, soustache_id_tache in sousresultache:
-                    if soustache_id_tache == id_tache :
+                for id_soustache, titre_soustache, soustache_id_tache, datesuppression_tache in sousresultache:
+                    if datesuppression_tache:
+                        pass
+                    elif soustache_id_tache == id_tache :
                         self.afficherTache(id_soustache, f'|=>{titre_soustache}', soustache_id_tache)
 
         curseur.close()
@@ -107,53 +110,66 @@ class Principal(QWidget):
 
     def supprimer(self, id_tache, soustache_id_tache):
         if not soustache_id_tache: #Tache
-            Supprimer(id_tache, self.cnx).exec()
+            Supprimer(id_tache, soustache_id_tache, self.cnx).exec()
             self.actualiser()
 
         else: #Sous-tache
-            print(f'Sous tache')
+            Supprimer(id_tache, soustache_id_tache, self.cnx).exec()
+            self.actualiser()
 
     def quitter(self):
         self.cnx.close()
         QCoreApplication.exit(0)
 
 class Supprimer(QDialog):
-    def __init__(self, id_tache, cnx):
+    def __init__(self, id_tache, soustache_id_tache, cnx):
         super().__init__()
 
         self.setWindowTitle("Supprimer")
 
         grid = QGridLayout()
         self.setLayout(grid)
-        self.resize(300, 350)
+        self.resize(300, 150)
 
         self.titre = QLabel("Supprimer la tâche")
         self.confirmer = QPushButton("Confirmer")
+        self.annuler = QPushButton("Annuler")
 
         grid.addWidget(self.titre)
         grid.addWidget(self.confirmer)
+        grid.addWidget(self.annuler)
 
         self.id_tache:str = id_tache
         self.cnx = cnx
+        self.soustache_id_tache = soustache_id_tache
 
         self.confirmer.clicked.connect(self.conf)
+        self.annuler.clicked.connect(self.stop)
 
     def conf(self):
         try :
             curseur = self.cnx.cursor()
-            curseur.execute(f'UPDATE taches SET datesuppression_tache = NOW() WHERE id_tache = "{self.id_tache}";')
+            if not self.soustache_id_tache:
+                curseur.execute(f'UPDATE taches SET datesuppression_tache = NOW() WHERE id_tache = "{self.id_tache}";')
+                curseur.execute(f'UPDATE soustaches SET datesuppression_soustache = NOW() '
+                                f'WHERE soustache_id_tache = "{self.id_tache}";')
+            else:
+                curseur.execute(f'UPDATE soustaches SET datesuppression_soustache = NOW() '
+                            f'WHERE id_soustache = "{self.id_tache}";')
             self.cnx.commit()
             curseur.close()
+
+            msg = QMessageBox()
+            msg.setWindowTitle("Confirmation")
+            msg.setText("Tache correctement supprimée")
+            msg.exec()
+
             self.close()
         except Exception as E:
             print(E)
 
-
-
-
-
-
-
+    def stop(self):
+       self.close()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
