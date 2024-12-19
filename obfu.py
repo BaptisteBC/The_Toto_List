6cb92a3d59bb56ced43ad513c0c667cafd6ea60f
macset <c.mourot2004@gmail.com>
2024-12-19 13:28:48 +0100

    ajout AES custom lib de Timothée

--- obfu.py
+++ obfu.py
@@ -1,6 +1,17 @@
 import pymysql
 import socket
 
+from lib.custom import AEScipher, AESsocket
+
+
+
+# Connexion au serveur
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('localhost', 12345))
+
+# Upgrade la connexion à une connexion sécurisée avec AES et Diffie-Hellman
+client_socket = AESsocket(client_socket, is_server=False)
+
 # Dictionnaire de mappage des tables
 mtables = {
     "utilisateur": "xner1etn",
@@ -44,9 +55,17 @@ def start_server():
     host = '0.0.0.0'  # Écouter sur toutes les interfaces réseau
     port = 40000
 
+    # Initialisation du serveur
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
-    server_socket.listen(5)
+    server_socket.listen(1)
+    print("Serveur en attente de connexion...")
+
+    client_socket, addr = server_socket.accept()
+    print(f"Connexion de {addr}")
+
+    # Upgrade la connexion à une connexion sécurisée avec AES et Diffie-Hellman
+    client_socket = AESsocket(client_socket, is_server=True)
 
     print("ok")
 
