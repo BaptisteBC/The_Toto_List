8f251510d3b878f2624ddcf862816835b0e8215b
BaptisteBC <@>
2024-12-21 09:02:07 +0100

    Affichage des détails d'une soustache avec tache parent

--- AfficherTache.py
+++ AfficherTache.py
@@ -1,22 +1,17 @@
 import sys
 
 import pymysql
-
-from PyQt5.QtCore import QCoreApplication, Qt, QDate
+from PyQt5.QtCore import QCoreApplication
 from PyQt5.QtWidgets import QWidget, QGridLayout, QApplication, QLabel, QPushButton, \
-    QListWidget, QLayout, QHBoxLayout, QListWidgetItem, QDialog, QFormLayout, QLineEdit, QDateTimeEdit, QCheckBox, \
-    QComboBox, QDialogButtonBox, QDateEdit, QMessageBox
-
-
-def tache_cliquee():
-    print("ça fonctionne !")
-
+    QListWidget, QHBoxLayout, QListWidgetItem, QDialog, QDateTimeEdit, QDateEdit
 
 
 class Principal(QWidget):
-    """Fenêtre principale d'affichage des tâches.
-    Prends en argument les infos de connexion à la BDD"""
+    """
+    Client d'affichage principal qui contient les fonctions principales d'affichage des tâches
 
+
+    """
     def __init__(self, host:str='127.0.0.1', user:str='root', password='toto', database:str='thetotodb'):
         super().__init__()
 
@@ -27,50 +22,52 @@ class Principal(QWidget):
         self.resize(500, 550)
 
         self.titre = QLabel("Mes tâches")
-        self.bouton_actualiser = QPushButton("Actualiser")
         self.taches = QListWidget()
         self.bouton_quitter = QPushButton("Quitter")
+        self.bouton_actualiser = QPushButton("Actualiser")
 
         grid.addWidget(self.titre)
         grid.addWidget(self.bouton_actualiser)
         grid.addWidget(self.taches)
         grid.addWidget(self.bouton_quitter)
 
-        self.bouton_actualiser.clicked.connect(self.actualiser)
         self.bouton_quitter.clicked.connect(self.quitter)
+        self.bouton_actualiser.clicked.connect(self.actualiser)
 
-        self.cnx = pymysql.connect(host=host, user=user, password=password, database=database)
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
 
         self.actualiser()
 
     def actualiser(self):
         """Fonction d'actualisation des tâches.
-        Lorsque le bouton est cliqué, effectue une requête dans la table tâche et récupère le champ 'titre_tache' pour \
+        Lorsue le bouton est cliqué, effectue une requête dans la table tâche et récupère le champ 'titre_tache' pour \
         l'afficher dans la fenêtre principale."""
 
+        self.cnx = pymysql.connect(host=self.host, user=self.user, password=self.password, database=self.database)
         curseur = self.cnx.cursor()
 
         # Extraction de tous les titres des taches principales
-        curseur.execute("SELECT id_tache, titre_tache, datesuppression_tache FROM taches;")
+        curseur.execute("SELECT id_tache, titre_tache FROM taches;")
         resultache = curseur.fetchall()
 
         # Extraction de tous les titres des sous taches
         curseur.execute("SELECT id_soustache, titre_soustache, soustache_id_tache FROM soustaches;")
         sousresultache = curseur.fetchall()
 
+        curseur.close()
+
         self.taches.clear()
 
-        for id_tache, titre_tache, datesuppression_tache in resultache:
-            if datesuppression_tache :
-                pass
-            else:
-                self.afficherTache(id_tache, titre_tache)
+        for id_tache, titre_tache in resultache:
 
-                for id_soustache, titre_soustache, soustache_id_tache in sousresultache:
-                    if soustache_id_tache == id_tache :
-                        self.afficherTache(id_soustache, f'|=>{titre_soustache}', soustache_id_tache)
+            self.afficherTache(id_tache, titre_tache)
 
-        curseur.close()
+            for id_soustache, titre_soustache, soustache_id_tache in sousresultache:
+                if soustache_id_tache == id_tache :
+                    self.afficherTache(id_soustache, titre_soustache, soustache_id_tache)
 
     def afficherTache(self, id_tache, titre_tache, soustache_id_tache=None):
         item = QListWidgetItem()
@@ -85,35 +82,40 @@ class Principal(QWidget):
         layout.addWidget(suppr)
         layout.addStretch()
 
-        detail.clicked.connect(lambda: self.detail(id_tache, soustache_id_tache, titre_tache))
-        suppr.clicked.connect(lambda : self.supprimer(id_tache, soustache_id_tache))
+        detail.clicked.connect(lambda: self.detail(id_tache, soustache_id_tache))
+        suppr.clicked.connect(lambda: self.supprimer(id_tache, soustache_id_tache))
 
         item.setSizeHint(tache.sizeHint())
 
         self.taches.addItem(item)
         self.taches.setItemWidget(item, tache)
 
-    def detail(self, id_tache, soustache_id_tache, titre_tache):
-        try :
-            Detail(id_tache, soustache_id_tache, titre_tache, self.cnx).exec()
-        except Exception as E:
-            print(E)
+    def detail(self, id_tache, soustache_id_tache):
+        if soustache_id_tache:
+            curseur = self.cnx.cursor()
 
-    def supprimer(self, id_tache, soustache_id_tache):
-        if not soustache_id_tache: #Tache
-            Supprimer(id_tache, self.cnx).exec()
-            self.actualiser()
+            curseur.execute(f'SELECT titre_soustache, description_soustache, datecreation_soustache, '
+                            f'datefin_soustache, statut_soustache, daterappel_soustache FROM soustaches '
+                            f'WHERE id_soustache = {id_tache};')
+            soustache = curseur.fetchall()
+            try:
+                curseur.execute(f'SELECT titre_tache FROM taches WHERE id_tache = {soustache_id_tache};')
+                tache_parent = curseur.fetchone()
+            except Exception as E:
+                print(E)
+            curseur.close()
 
-        else: #Sous-tache
-            print(f'Sous tache')
+            for (titre_soustache, description_soustache, datecreation_soustache, datefin_soustache, statut_soustache,
+                daterappel_soustache) in soustache:
+                Detail(titre_soustache, description_soustache, datecreation_soustache, datefin_soustache,
+                statut_soustache, daterappel_soustache, soustache_id_tache, tache_parent[0]).exec()
 
     def quitter(self):
         self.cnx.close()
-        QCoreApplication.exit(0)
-
+        QCoreApplication.exit()
 
 class Detail(QDialog):
-    def __init__(self, id_tache, soustache_id_tache, titre_tache, cnx):
+    def __init__(self, titre, description, datecreation, datefin, statut, daterappel, soustache_id_tache, tache_parent=None):
         super().__init__()
 
         self.setWindowTitle("Détail")
@@ -122,32 +124,33 @@ class Detail(QDialog):
         self.setLayout(grid)
         self.resize(300, 250)
 
-        self.titre = QLabel(f'Titre : {titre_tache}')
-        self.description = QLabel()
+        self.titre = QLabel(f'Titre : {titre}')
+        self.description = QLabel(f'Description : {description}')
+        self.datecreation = QLabel(f'Date de création : {datecreation}')
+        self.datefin = QLabel(f'Date de fin : {datefin}')
+
+        if statut == 1:
+            self.statut = QLabel("Statut : En cours")
+        else:
+            self.statut = QLabel("Statut : Terminée")
+
+        self.daterappel = QLabel(f'Date de rappel : {daterappel}')
         self.confirmer = QPushButton("OK")
 
 
         grid.addWidget(self.titre)
+        if soustache_id_tache:
+            self.tache_parent = QLabel(f'Tache parent : {tache_parent}')
+            grid.addWidget(self.tache_parent)
+        grid.addWidget(self.statut)
         grid.addWidget(self.description)
+        grid.addWidget(self.datecreation)
+        grid.addWidget(self.datefin)
+
+        grid.addWidget(self.daterappel)
         grid.addWidget(self.confirmer)
 
-        self.id_tache:str = id_tache
-        self.cnx = cnx
-        self.soustache_id_tache = soustache_id_tache
-
         self.confirmer.clicked.connect(self.stop)
-
-        self.get_details()
-
-    def get_details(self):
-        if self.soustache_id_tache:
-            curseur = self.cnx.cursor()
-            curseur.execute(f'SELECT titre_soustache, description_soustache, datecreation_soustache, datefin_soustache,'
-                            f'statut_soustache, daterappel_soustache FROM soustaches '
-                            f'WHERE id_soustache = {self.id_tache};')
-        else:
-            print("tache")
-
 
     def stop(self):
        self.close()
@@ -183,13 +186,6 @@ class Supprimer(QDialog):
             self.close()
         except Exception as E:
             print(E)
-
-
-
-
-
-
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
