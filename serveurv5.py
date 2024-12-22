b5512416b985d913ef6d021c4a6b979f7a69532a
Thibaut COSENZA <113136288+Thibautcsnz@users.noreply.github.com>
2024-12-22 22:49:49 +0100

    Il faut réussir à faire fonctionner les boutons (Détails, Modifier et ajout sous tache) et également le bouton actualiser

--- serveurv5.py
+++ serveurv5.py
@@ -5,13 +5,115 @@ import pymysql
 import time
 import sys,os
 import cryptocode
-import datetime
+from datetime import datetime
 import hashlib
 import re
 import bcrypt
 import pymysql.cursors
 from lib.custom import AESsocket  # Importation de la classe personnalisée pour le chiffrement AES
 import json
+
+# Fonction de journalisation
+def journalisation(utilisateurId: int, typeEvenement: str):
+    """Enregistre un événement dans la table de journalisation.
+
+    :param utilisateurId: Identifiant de l'utilisateur.
+    :type utilisateurId: int
+    :param typeEvenement: Chaine d'evenement, il est conseiller d'utiliser la fonction "creerChaineEvenement".
+    :type typeEvenement: str
+    :raises pymysql.MySQLError: Erreur lors de l'insertion dans la base de données.
+    :return: None
+    :rtype: None
+    """
+    try:
+        connexion = pymysql.connect(
+            host="127.0.0.1",
+            user="root",
+            password="toto",
+            database="TheTotoDB"
+        )
+
+        curseur = connexion.cursor()
+        dateEvenement = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        requete = """
+        INSERT INTO journalisation (journalisation_id_utilisateur, type_evenement, date_evenement)
+        VALUES (%s, %s, %s)
+        """
+        valeurs = (utilisateurId, typeEvenement, dateEvenement)
+        curseur.execute(requete, valeurs)
+        connexion.commit()
+        print("Informations insérées dans la table 'journalisation'.")
+
+    except pymysql.MySQLError as erreur:
+        print(f"Erreur d'insertion : {erreur}")
+
+    finally:
+        if curseur is not None:
+            curseur.close()
+        if connexion is not None:
+            connexion.close()
+
+def creerChaineEvenement(premierX: int, deuxiemeX: int, troisiemeX: int, listeY, listeZ):
+    """Crée une chaîne d'événements formatée pour la fonction journalisation.
+
+    :param premierX: Type d'évènement : 0 Ajout ; 1 Modification ; 2 Suppression.
+    :type premierX: int
+    :param deuxiemeX:  Objet affecté par l'évènement : 0 Utilisateur ; 1 Tache ; 2 Sous tâche ; 3 Liste ; 4 Group ; 5 Groupes_utilisateurs ; 6 Etiquettes ; 7 Etiquettes_elements
+    :type deuxiemeX: int
+    :param troisiemeX: Id de l'entrée affectée.
+    :type troisiemeX: int
+    :param listeY: Liste de noms ou d'indices de colonnes, selon le cas.
+    :type listeY: list | str
+    :param listeZ: Liste des valeurs modifiées dans l'ordre des N° de colonne.
+    :type listeZ: list
+    :raises pymysql.MySQLError: Erreur lors de la connexion à la base de données.
+    :raises ValueError: Si certaines colonnes dans listeY ne correspondent pas.
+    :return: Chaîne d’événements formatée.
+    :rtype: str | None
+    """
+    try:
+        connexion = pymysql.connect(
+            host="127.0.0.1",
+            user="root",
+            password="toto",
+            database="TheTotoDB"
+        )
+
+        curseur = connexion.cursor()
+
+        if isinstance(listeY, list):
+            if all(isinstance(element, str) for element in listeY):
+                tables = [
+                    "utilisateurs", "taches", "soustaches", "listes", "groupes",
+                    "groupes_utilisateurs", "etiquettes", "etiquettes_elements"
+                ]
+                nomTable = tables[deuxiemeX]
+                colonnesValides = []
+                for colonne in listeY:
+                    requete = f"SHOW COLUMNS FROM {nomTable} LIKE %s"
+                    curseur.execute(requete, (colonne,))
+                    resultat = curseur.fetchone()
+                    if resultat:
+                        colonnesValides.append(resultat[0])
+                if len(colonnesValides) != len(listeY):
+                    print("Certaines colonnes ne correspondent pas, annulation de l'opération.")
+                    return None
+                listeY = [colonnesValides.index(colonne) + 1 for colonne in colonnesValides]
+
+        listeYStr = ".".join(map(str, listeY)) if isinstance(listeY, list) else str(listeY)
+        zStr = ".".join(map(str, listeZ))
+        chaineEvenement = f"{premierX}.{deuxiemeX}.{troisiemeX}.{{{listeYStr}}}.{{{zStr}}}"
+
+        return chaineEvenement
+
+    except pymysql.MySQLError as erreur:
+        print(f"Erreur de connexion : {erreur}")
+
+    finally:
+        if curseur is not None:
+            curseur.close()
+        if connexion is not None:
+            connexion.close()
 
 class Server:
     def __init__(self):
@@ -372,11 +474,257 @@ class Server:
                 cursor.execute(sql, (userId, listId, taskTitle, taskDescription, dueDate, status, reminderDate))
                 self.dbConnection.commit()
 
+
+                # Journalisation de la création de la tâche
+                taskId = cursor.lastrowid  # ID de la tâche insérée
+                listeY = ["titre_tache", "description_tache", "datefin_tache", "statut_tache", "daterappel_tache"]
+                listeZ = [taskTitle, taskDescription, dueDate, status, reminderDate]
+
+                chaineEvenement = creerChaineEvenement(0, 1, taskId, listeY, listeZ)
+                if chaineEvenement:
+                    journalisation(userId, chaineEvenement)
+
                 return "Tâche créée avec succès."
 
         except Exception as e:
             print(f"Erreur MySQL: {e}")
             return "Erreur lors de la création de la tâche."
+
+    def interpretCommand(self, command):
+        """
+        Interprète et exécute une commande reçue du client.
+
+        :param command: Commande envoyée par le client
+        :type command: str
+        :return: Réponse à retourner au client
+        :rtype: str
+        """
+        try:
+            if command.startswith("MODIF_TACHE"):
+                details = command.split("|")[1:]
+                return self.modifTache(*details)
+
+            elif command.startswith("GET_tache"):
+                idTache = command.split(":")[1]
+                return self.getTache(idTache)
+
+            elif command.startswith("GET_sousTache"):
+                idSousTache = command.split(":")[1]
+                return self.getSousTache(idSousTache)
+
+            elif command.startswith("modifSousTache"):
+                details = command.split("|")[1:]
+                return self.modifSousTache(*details)
+
+            elif command.startswith("validationSousTache"):
+                details = command.split(":")[1:]
+                return self.validationSousTache(*details)
+
+            elif command.startswith("validation"):
+                details = command.split(":")[1:]
+                return self.validation(*details)
+
+            if command.startswith("creationSousTache"):
+                details = command.split("|")[1:]
+                return self.creationSousTache(*details)
+
+            else:
+                return "Commande inconnue."
+
+        except Exception as e:
+            print(f"Erreur dans l'interprétation de la commande: {e}")
+            return "Erreur serveur."
+
+
+    def getTache(self, idTache):
+        """
+        Récupère les informations sur une tâche spécifique.
+
+        :param idTache: ID de la tâche à récupérer
+        :type idTache: str
+        :return: JSON contenant les informations sur la tâche (avec datetime sérialisé)
+        :rtype: str
+        :raises Exception: Si une erreur MySQL se produit
+        """
+        try:
+            with self.dbConnection.cursor() as cursor:
+                cursor.execute(
+                    "SELECT titre_tache, description_tache, datefin_tache, recurrence_tache, daterappel_tache FROM taches WHERE id_tache = %s;",
+                    (idTache,)
+                )
+
+                results = cursor.fetchall()
+
+                if results:
+                    serialized_results = [[ value.strftime('%Y-%m-%d %H:%M:%S') if isinstance(value, datetime) else value for value in row ] for row in results ]
+                    return json.dumps(serialized_results)
+                else:
+                    return json.dumps([])
+
+        except Exception as e:
+            print(f"Erreur MySQL: {e}")
+            return json.dumps({"error": "Erreur MySQL."})
+
+    def modifTache(self, idTache, titre, description, dateFin, recurrence, dateRappel):
+        """
+        Modifie une tâche existante.
+
+        :param idTache: ID de la tâche à modifier
+        :type idTache: str
+        :param titre: Nouveau titre de la tâche
+        :type titre: str
+        :param description: Nouvelle description de la tâche
+        :type description: str
+        :param dateFin: Nouvelle date de fin de la tâche
+        :type dateFin: str
+        :param recurrence: Nouvelle récurrence de la tâche
+        :type recurrence: str
+        :param dateRappel: Nouvelle date de rappel de la tâche, peut être 'NULL'
+        :type dateRappel: str, optional
+        :return: Message de succès ou d'erreur
+        :rtype: str
+        :raises Exception: Si une erreur MySQL se produit
+        """
+        try:
+
+            with self.dbConnection.cursor() as cursor:
+                cursor.execute("""
+                    UPDATE taches SET titre_tache = %s, description_tache = %s, datefin_tache = %s, recurrence_tache = %s, daterappel_tache = %s
+                    WHERE id_tache = %s;
+                """, (titre, description, dateFin, recurrence, dateRappel if dateRappel != 'NULL' else None, idTache))
+                self.dbConnection.commit()
+                return "Tâche modifiée avec succès."
+        except Exception as e:
+            print(f"Erreur MySQL: {e}")
+            return "Erreur MySQL."
+
+    def getSousTache(self, idSousTache):
+        """
+        Récupère les informations sur une sous-tâche spécifique.
+
+        :param idSousTache: ID de la sous-tâche à récupérer
+        :type idSousTache: str
+        :return: JSON contenant les informations sur la sous-tâche (avec datetime sérialisé)
+        :rtype: str
+        :raises Exception: Si une erreur MySQL se produit
+        """
+        try:
+            with self.dbConnection.cursor() as cursor:
+                cursor.execute(
+                    "SELECT titre_soustache, description_soustache, datefin_soustache, daterappel_soustache FROM soustaches WHERE id_soustache = %s;", (idSousTache,)
+                )
+                results = cursor.fetchall()
+                if results:
+                    serialized_results = [[ value.strftime('%Y-%m-%d %H:%M:%S') if isinstance(value, datetime) else value for value in row ] for row in results ]
+                    return json.dumps(serialized_results)
+                else:
+                    return json.dumps([])
+
+        except Exception as e:
+            print(f"Erreur MySQL: {e}")
+            return json.dumps({"error": "Erreur MySQL."})
+    def modifSousTache(self, idSousTache, titre, description, dateFin, dateRappel):
+        """
+        Modifie une sous-tâche existante.
+
+        :param idSousTache: ID de la sous-tâche à modifier
+        :type idSousTache: str
+        :param titre: Nouveau titre de la sous-tâche
+        :type titre: str
+        :param description: Nouvelle description de la sous-tâche
+        :type description: str
+        :param dateFin: Nouvelle date de fin de la sous-tâche
+        :type dateFin: str
+        :param dateRappel: Nouvelle date de rappel de la sous-tâche, peut être 'NULL'
+        :type dateRappel: str, optional
+        :return: Message de succès ou d'erreur
+        :rtype: str
+        :raises Exception: Si une erreur MySQL se produit
+        """
+        try:
+            with self.dbConnection.cursor() as cursor:
+                cursor.execute("""
+                    UPDATE soustaches SET titre_soustache = %s, description_soustache = %s, datefin_soustache = %s, daterappel_soustache = %s
+                            WHERE id_soustache = %s;
+                """, (titre, description, dateFin,dateRappel if dateRappel != 'NULL' else None, idSousTache))
+                self.dbConnection.commit()
+                return "Sous tâche modifiée avec succès."
+        except Exception as e:
+            print(f"Erreur MySQL: {e}")
+            return "Erreur MySQL."
+
+    def creationSousTache(self, soustache_id_tache, titre_soustache, description_soustache, datefin_soustache, daterappel_soustache):
+        """
+        Crée une nouvelle sous-tâche.
+
+        :param soustache_id_tache: ID de la tâche parent
+        :type soustache_id_tache: str
+        :param titre_soustache: Titre de la nouvelle sous-tâche
+        :type titre_soustache: str
+        :param description_soustache: Description de la nouvelle sous-tâche
+        :type description_soustache: str
+        :param datefin_soustache: Date de fin de la nouvelle sous-tâche
+        :type datefin_soustache: str
+        :param daterappel_soustache: Date de rappel de la nouvelle sous-tâche, peut être 'NULL'
+        :type daterappel_soustache: str, optional
+        :return: Message de succès ou d'erreur
+        :rtype: str
+        :raises Exception: Si une erreur MySQL se produit
+        """
+        try:
+            with self.dbConnection.cursor() as cursor:
+                cursor.execute("""
+                    INSERT INTO soustaches (soustache_id_tache, titre_soustache, description_soustache, datefin_soustache, daterappel_soustache, datecreation_soustache, statut_soustache)
+                            VALUES (%s, %s, %s, %s, %s, NOW(), 0);
+                        """, (soustache_id_tache, titre_soustache, description_soustache, datefin_soustache,
+                              daterappel_soustache if daterappel_soustache != 'NULL' else None) )
+                self.dbConnection.commit()
+                return "Sous tâche crée avec succès."
+        except Exception as e:
+            print(f"Erreur MySQL: {e}")
+            return "Erreur MySQL."
+
+    def validation(self, idTache, etatValidation):
+        """
+        Modifie le statut de validation d'une tâche.
+
+        :param idTache: ID de la tâche à valider
+        :type idTache: str
+        :param etatValidation: Nouveau statut de validation
+        :type etatValidation: str
+        :return: Message de succès ou d'erreur
+        :rtype: str
+        :raises Exception: Si une erreur MySQL se produit
+        """
+        try:
+            with self.dbConnection.cursor() as cursor:
+                cursor.execute("UPDATE taches SET statut_tache = %s WHERE id_tache = %s;", (etatValidation, idTache))
+                self.dbConnection.commit()
+                return "Validation modifier avec succes"
+        except Exception as e:
+            print(f"Erreur MySQL: {e}")
+            return "Erreur MySQL."
+
+    def validationSousTache(self, idSousTache, etatValidationSousTache):
+        """
+        Modifie le statut de validation d'une sous-tâche.
+
+        :param idSousTache: ID de la sous-tâche à valider
+        :type idSousTache: str
+        :param etatValidationSousTache: Nouveau statut de validation
+        :type etatValidationSousTache: str
+        :return: Message de succès ou d'erreur
+        :rtype: str
+        :raises Exception: Si une erreur MySQL se produit
+        """
+        try:
+            with self.dbConnection.cursor() as cursor:
+                cursor.execute("UPDATE soustaches SET statut_soustache = %s WHERE id_soustache = %s;", (etatValidationSousTache, idSousTache))
+                self.dbConnection.commit()
+                return "Validation sous tache modifier avec succes"
+        except Exception as e:
+            print(f"Erreur MySQL: {e}")
+            return "Erreur MySQL."
 
 if __name__ == '__main__':
 
