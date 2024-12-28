d04a47885daac1b93a8efa701b984a52eddd56ca
iPresing <timothee.trepier@uha.fr>
2024-12-28 18:56:31 +0100

    Docker : Suppression des informations de debogage.

--- Docker/client_app/docker-entrypoint.sh
+++ Docker/client_app/docker-entrypoint.sh
@@ -30,8 +30,6 @@ fi
 # Effectuer la première requête
 result1=$(curl -s https://version.pnoutlet.site/version --connect-timeout 5)
 
-echo "$result1"
-
 # Vérifier la taille de la réponse
 if [ ${#result1} -ne 32 ]; then
     echo "Il n\'est pas possible d\'atteindre le site de versionnage, veuillez vérifier votre connectivité"
@@ -49,7 +47,6 @@ if [[ ! -d "$PROJECT_DIR" || ! "$(ls -A $PROJECT_DIR)" ]]; then
 else
    echo "Vérification de la version actuelle..."
    result2=$(find "$PROJECT_DIR" -type f ! -path '*/.git/*' ! -path '*/__pycache__/*' -exec md5sum {} \; | awk '{print $1}' | sort | md5sum | cut -d " " -f 1)
-   echo "$result2"
    if [[ "$result1" == "$result2" ]]; then
        echo "Version à jour ! Lancement de l'application..."
        python "${PROJECT_DIR}/${APP_NAME}"
